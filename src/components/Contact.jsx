09a2fc0ab45f2bcec308599b907d93f2223cca92
Samuel Oluwatimilehin Aluko <adedayotimilehin10@gmail.com>
2025-11-23 23:43:21 +0100

    edited contact form to implement cool functionalities

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,6 +22,7 @@ const Contact = () => {
     form.email.trim() !== "" &&
     form.message.trim() !== "";
 
+    let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   const handleChange = (e) => {
     const { target } = e;
@@ -35,86 +36,114 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setLoading(true);
+    if(regex.test(form.email)) {
+ setLoading(true);
 
-    emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          name: form.name,
-          to_name: "Samuel Aluko",
-          email: form.email,
-          to_email: "adedayotimilehin10@gmail.com",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setLoading(false);
-          Swal.fire({
-            title: "Message Successfully Sent! 🎉",
-            html: `Thanks for reaching out, ${form.name}. <br />I’ll get back to you ASAP!`,
-            icon: "success",
+ emailjs
+   .send(
+     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+     {
+       name: form.name,
+       to_name: "Samuel Aluko",
+       email: form.email,
+       to_email: "adedayotimilehin10@gmail.com",
+       message: form.message,
+     },
+     import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+   )
+   .then(
+     () => {
+       setLoading(false);
+       Swal.fire({
+         title: "Message Successfully Sent! 🎉",
+         html: `Thanks for reaching out, ${form.name}. <br />I’ll get back to you ASAP!`,
+         icon: "success",
 
-            // ---- CUSTOM STYLING ----
-            background: "#1f1f1f",
-            color: "#ffffff",
-            iconColor: "#4f46e5",
-            confirmButtonText: "Okay!",
-            confirmButtonColor: "#4f46e5",
-            buttonsStyling: false,
-            customClass: {
-              popup: "rounded-xl shadow-2xl",
-              confirmButton:
-                "px-6 py-2 rounded-lg bg-primary text-white font-semibold hover:bg-primary/80 transition-all",
-            },
+         // ---- CUSTOM STYLING ----
+         background: "#1f1f1f",
+         color: "#ffffff",
+         iconColor: "#4f46e5",
+         confirmButtonText: "Okay",
+         confirmButtonColor: "#4f46e5",
+         buttonsStyling: false,
+         customClass: {
+           popup: "rounded-xl shadow-2xl",
+           confirmButton:
+             "px-6 py-2 rounded-lg bg-primary text-white font-semibold hover:bg-primary/80 transition-all",
+         },
 
-            // ---- ANIMATION ----
-            showClass: {
-              popup: "animate__animated animate__fadeInUp",
-            },
-            hideClass: {
-              popup: "animate__animated animate__fadeOutDown",
-            },
-          });
+         // ---- ANIMATION ----
+         showClass: {
+           popup: "animate__animated animate__fadeInUp",
+         },
+         hideClass: {
+           popup: "animate__animated animate__fadeOutDown",
+         },
+       });
 
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
-          Swal.fire({
-            title: "Oops! 😢",
-            html: `Something went wrong, ${form.name}. <br />Please try again so that I can attend to you ASAP!.`,
-            icon: "error",
+       setForm({
+         name: "",
+         email: "",
+         message: "",
+       });
+     },
+     (error) => {
+       setLoading(false);
+       console.error(error);
+       Swal.fire({
+         title: "Message Not Sent!",
+         html: `Something went wrong, ${form.name}. <br />Please try again so that I can attend to you ASAP!`,
+         icon: "error",
 
-            background: "#1f1f1f",
-            color: "#ffffff",
-            iconColor: "#ef4444",
-            confirmButtonText: "Retry",
-            confirmButtonColor: "#ef4444",
-            buttonsStyling: false,
-            customClass: {
-              popup: "rounded-xl shadow-2xl",
-              confirmButton:
-                "px-6 py-2 rounded-lg bg-red-500 text-white font-semibold hover:bg-red-600 transition-all",
-            },
+         background: "#1f1f1f",
+         color: "#ffffff",
+         iconColor: "#ef4444",
+         confirmButtonText: "Retry",
+         confirmButtonColor: "#ef4444",
+         buttonsStyling: false,
+         customClass: {
+           popup: "rounded-xl shadow-2xl",
+           confirmButton:
+             "px-6 py-2 rounded-lg bg-red-500 text-white font-semibold hover:bg-red-600 transition-all",
+         },
 
-            showClass: {
-              popup: "animate__animated animate__fadeInUp",
-            },
-            hideClass: {
-              popup: "animate__animated animate__fadeOutDown",
-            },
-          });
-        }
-      );
+         showClass: {
+           popup: "animate__animated animate__fadeInUp",
+         },
+         hideClass: {
+           popup: "animate__animated animate__fadeOutDown",
+         },
+       });
+     }
+   );
+    }else{
+     Swal.fire({
+       title: "Wrong email format!",
+       html: `Please fill in a valid email address, ${form.name}.`,
+       icon: "error",
+
+       background: "#1f1f1f",
+       color: "#ffffff",
+       iconColor: "#ef4444",
+       confirmButtonText: "Okay",
+       confirmButtonColor: "#ef4444",
+       buttonsStyling: false,
+       customClass: {
+         popup: "rounded-xl shadow-2xl",
+         confirmButton:
+           "px-6 py-2 rounded-lg bg-red-500 text-white font-semibold hover:bg-red-600 transition-all",
+       },
+
+       showClass: {
+         popup: "animate__animated animate__fadeInUp",
+       },
+       hideClass: {
+         popup: "animate__animated animate__fadeOutDown",
+       },
+     });
+    }
+   
   };
 
   return (
@@ -176,7 +205,7 @@ const Contact = () => {
             className={`bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary 
     ${
       !isFormValid || loading
-        ? "opacity-30 cursor-not-allowed"
+        ? "opacity-70 cursor-not-allowed"
         : "opacity-100 cursor-pointer"
     }`}
           >
